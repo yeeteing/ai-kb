c571e468f975329aad0ae0bdcd789150479d8841
Yee Teing Lo <36290308+yeeteing@users.noreply.github.com>
2025-10-05 13:07:24 -0400

    Update session.py

--- app/db/session.py
+++ app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine #makes a connection “engine” to your da
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
-#Creates a database engine using the connection string (Postgres in your case).
+#Creates a database engine using the connection string
 engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True) #pool_pre_ping=True makes sure SQLAlchemy checks if a DB connection is still alive before reusing it
 #SessionLocal is a factory for creating new DB Session objects that are bound to the engine.
 #autocommit=False => you control when to commit changes.
